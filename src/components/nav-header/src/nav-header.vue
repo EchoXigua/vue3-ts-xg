9605c940006b94dc68b4e82b5fddee355be3dacf
EchoXigua <643589668@qq.com>
2022-11-07 22:49:32 +0800

    header doing

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
-    <el-icon :size="16" style="cursor: pointer" @click="FoldClick">
+  <div class="nav-header">
+    <el-icon class="fold-menu" :size="30" @click="FoldClick">
       <Expand v-if="isFold"></Expand>
       <Fold v-else></Fold>
     </el-icon>
-    header111
+    <div class="content">
+      <div>面包靴</div>
+      <div>
+        <user-info></user-info>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { Fold, Expand } from '@element-plus/icons-vue'
+import UserInfo from './user-info.vue'
 export default defineComponent({
-  components: { Fold, Expand },
+  components: { Fold, Expand, UserInfo },
   emits: ['foldChange'],
   setup(props, { emit }) {
     const isFold = ref(false)
@@ -27,4 +33,21 @@ export default defineComponent({
   }
 })
 </script>
-<style scoped></style>
+<style lang="less" scoped>
+.nav-header {
+  display: flex;
+  width: 100%;
+
+  .fold-menu {
+    cursor: pointer;
+    font-size: 30px;
+  }
+  .content {
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+  }
+}
+</style>
---
+++ src/components/nav-header/src/user-info.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <el-avatar size="small" :icon="UserFilled" />
+        <span>用户名</span>
+        <el-icon>
+          <arrow-down />
+        </el-icon>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item
+            ><el-icon><SwitchButton /></el-icon>退出登录</el-dropdown-item
+          >
+          <el-dropdown-item divided
+            ><el-icon><User /></el-icon>用户信息</el-dropdown-item
+          >
+          <el-dropdown-item
+            ><el-icon><Tools /></el-icon>系统管理</el-dropdown-item
+          >
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+  ArrowDown,
+  SwitchButton,
+  User,
+  Tools,
+  UserFilled
+} from '@element-plus/icons-vue'
+export default defineComponent({
+  components: { ArrowDown, SwitchButton, User, Tools },
+  setup() {
+    return {
+      UserFilled
+    }
+  }
+})
+</script>
+<style scoped>
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
