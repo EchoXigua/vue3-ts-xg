a9683ec62acd25eeb37f4d6968a9dee4513c2512
yal <98389358+EchoXigua@users.noreply.github.com>
2022-12-23 10:25:29 +0800

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,8 +27,9 @@ echo 'name is: '$GH_NAME
 echo 'sitesource is: '$siteSource
 
 # 切换gh-pages分支
-if git rev-parse --verify origin/gh-pages >/
-dev/null 2>&1; then
+if git rev-parse --verify origin/gh-pages 
+# dev/null 2>&1
+  ; then
   git checkout gh-pages
   # 删除旧的文件内容
   git rm -rf .
